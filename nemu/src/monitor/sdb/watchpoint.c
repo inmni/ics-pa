b90f257a70a5ecdb09323be7de2d8ed95b2ecc57
tracer-ics2022 <tracer@njuics.org>
2022-09-18 13:20:25 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:20:25 up  4:00,  6 users,  load average: 0.04, 0.14, 0.27

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -22,12 +22,13 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  char EXPR[32];
+  int currValue,targetValue;
 } WP;
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
-
+WP* find_wp(int n);
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
@@ -38,6 +39,41 @@ void init_wp_pool() {
   head = NULL;
   free_ = wp_pool;
 }
-
+void print_wp(WP* wp){
+	printf("NO:%d,ADDRESS:%s,CURRENT_VALUE:%d,TARGET_VALUE:%d\n",wp->NO,wp->EXPR,wp->currValue,wp->targetValue);
+}
 /* TODO: Implement the functionality of watchpoint */
-
+void wp_all_display(){
+	if(head==NULL){
+		printf("No watchpoints now!\n");
+		return;
+	}
+	WP* wp = head;
+	while(wp){
+		print_wp(wp);
+		wp = wp->next;
+	}
+}
+void wp_display(int n){
+	if(head==NULL){
+		printf("No watchpoints now!\n");
+		return;
+	}
+	WP* wp = find_wp(n);
+	if(wp){
+		print_wp(wp);
+		return;
+	}
+	printf("NO.%d watchpoint does not exist or not active",n);
+	return;
+}
+WP* find_wp(int n){
+	WP* wp = head;
+	while(wp){
+		if(wp->NO==n){
+			return wp;
+		}
+		wp = wp->next;
+	}
+	return NULL;
+}
