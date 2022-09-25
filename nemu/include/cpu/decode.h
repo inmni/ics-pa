f6edb86bd1f72b7e226b8a1138f2e11760e3e447
tracer-ics2022 <tracer@njuics.org>
2022-09-25 16:15:32 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:15:32 up  2:35,  7 users,  load average: 0.72, 0.81, 0.88

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -92,7 +92,6 @@ finish:
   pattern_decode(pattern, STRLEN(pattern), &key, &mask, &shift); \
   if (((INSTPAT_INST(s) >> shift) & mask) == key) { \
     INSTPAT_MATCH(s, ##__VA_ARGS__); \
-		printf("before the instpat end"); \
     goto *(__instpat_end); \
   } \
 } while (0)
