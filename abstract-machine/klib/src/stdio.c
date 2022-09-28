c60eccb7a44fe8d591eeebcb073992234317b6b2
tracer-ics2022 <tracer@njuics.org>
2022-09-28 23:00:14 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:00:14 up  3:50,  8 users,  load average: 0.36, 1.06, 0.97

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,6 +14,38 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	while(*fmt){
+		if(*fmt!='%'){
+			*out++ = *fmt++;
+			continue;
+		}
+		fmt++;
+		if(*fmt=='d'){
+			int d = va_arg(ap, int);
+			char tmp[32]={0};
+			int i=0;
+			int tmp_d;
+			while(d>0){
+				tmp_d = d/10;
+				tmp[i] = d-((tmp_d<<3)+(tmp_d<<1))-'0';
+				d = tmp_d;
+				i++;
+			}
+			i--;
+			for(;i>=0;i--){
+				*out++=tmp[i];
+			}
+		}
+		else if(*fmt=='s'){
+			char *str = va_arg(ap, char *);
+			int len = strlen(str);
+			strcpy(out,str);
+			out+=len;
+		}
+		else{panic("No found format");}
+	}
   panic("Not implemented");
 }
 
