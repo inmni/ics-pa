5ca2c87415ca1435460ddc6bfac81b79afc8a58c
tracer-ics2022 <tracer@njuics.org>
2022-09-30 21:50:39 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:50:39 up 1 day,  1:04,  5 users,  load average: 3.44, 1.82, 4.27

--- nemu/src/monitor/sdb/trace.c
+++ nemu/src/monitor/sdb/trace.c
@@ -62,7 +62,7 @@ void init_ftrace(const char *elf_file){
 					printf("Get symbol table result:%d, offset:%d, size:%d\n", fr_r, sh->sh_offset, sh->sh_size);
 				}
 				else if(sh->sh_type == SHT_STRTAB){
-					str_table = (char *)malloc(sh->sh_size);
+					str_table = (char *)malloc(sh->sh_size+5);
 					fseek(file, sh->sh_offset, SEEK_SET);
 					fr_r = fread(str_table, 1, sh->sh_size, file);
 					printf("Get string table result:%d, offset:%d, size:%d\n", fr_r, sh->sh_offset, sh->sh_size);
