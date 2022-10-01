04e45c589bca07b0c26cc598771e48dd5e4bbb3c
tracer-ics2022 <tracer@njuics.org>
2022-10-01 22:36:15 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:36:15 up 2 days,  1:49,  9 users,  load average: 0.94, 0.54, 0.66

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,6 +6,7 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = (bool)((*(volatile uint8_t *)KBD_ADDR)>>7);
   kbd->keycode = ((*(volatile uint32_t *)KBD_ADDR)<<1)>>1;
-	if(kbd->keydown||kbd->keycode)
-	printf("down:%d code:%d\n",kbd->keydown,kbd->keycode);
+	int tmp = *(volatile uint32_t *)KBD_ADDR;
+	if(tmp)
+	printf("all:%08x",tmp);
 }
