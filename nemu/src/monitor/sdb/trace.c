cfe4deec2e03f7cd7e7b4d355f73cad45971b01b
tracer-ics2022 <tracer@njuics.org>
2022-10-01 11:42:14 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:42:14 up 1 day, 14:55,  5 users,  load average: 0.18, 0.11, 0.23

--- nemu/src/monitor/sdb/trace.c
+++ nemu/src/monitor/sdb/trace.c
@@ -79,7 +79,7 @@ void init_ftrace(const char *elf_file){
 		Elf32_Sym sym;
 		for(int i=0;i<sym_count;i++){
 				sym = sym_table[i];
-				printf("value:%08x, st_name:%d	",sym.st_value,sym.st_name);
+				printf("value:%08x, st_name:%d, st_info:%d	",sym.st_value,sym.st_name,sym.st_info);
 				if(sym.st_info!=STT_FUNC)continue;
 				printf("%s\n",str_table+sym.st_name);
 		}
