8a720446e1bf81faed3536fcc157127c3d9e4263
tracer-ics2022 <tracer@njuics.org>
2022-10-01 16:11:39 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:11:39 up 1 day, 19:25,  9 users,  load average: 0.56, 0.61, 0.49

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,30 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+#define match(tmp, fmt, ap, count) ({\
+memset(tmp,0,256);\
+if(*fmt!='%'){\
+		count = 1;\
+		*tmp++=*fmt++;\
+		*tmp=0;\
+}\
+else{\
+	fmt++;\
+	if(*fmt=='d'){\
+			count = itoa(va_arg(ap, int), tmp, 10)-tmp;\
+			fmt++;\
+	}\
+	else if(*fmt=='s'){\
+			tmp = va_arg(ap, char *);\
+			count = strlen(tmp);\
+			tmp++;\
+	}\
+	else{\
+			panic("No found format");return 0;\
+	}\
+}\
+\
+})
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -16,7 +39,14 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 int sprintf(char *out, const char *fmt, ...) {
 	va_list ap;
 	va_start(ap, fmt);
+	int move=0;
+	char tmp[256]={0};
 	while(*fmt){
+		char *head = tmp;
+		match(head, fmt, ap, move);
+		strcpy(out, tmp);
+		out+=move;
+	continue;	
 		if(*fmt!='%'){
 			*out++ = *fmt++;
 			continue;
