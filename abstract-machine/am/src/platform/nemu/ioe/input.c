a7a9aa0af64f68dbc90265afd1044c9fb986ca62
tracer-ics2022 <tracer@njuics.org>
2022-10-01 23:06:19 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:06:19 up 2 days,  2:19,  9 users,  load average: 0.61, 3.11, 2.54

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,8 +5,8 @@
 #define KEYCODE_MASK 0x7FFF
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = (bool)(((*(volatile uint32_t *)KBD_ADDR)&KEYDOWN_MASK)>>15);
-  kbd->keycode = ((*(volatile uint32_t *)KBD_ADDR)&KEYCODE_MASK);
+  kbd->keycode = (*(volatile uint32_t *)KBD_ADDR) & KEYCODE_MASK;
 	int tmp = *(volatile uint32_t *)KBD_ADDR;
 	if(tmp)
-	printf("all:%d\n",tmp);
+	printf("keydown:%d,keycode:%d\n",kbd->keydown,kbd->keycode);
 }
