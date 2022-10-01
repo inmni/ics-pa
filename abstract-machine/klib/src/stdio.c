21e0bc6039d969d4c4644a1b570f5e393c67e5fc
tracer-ics2022 <tracer@njuics.org>
2022-10-01 17:40:40 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:40:40 up 1 day, 20:54,  9 users,  load average: 0.02, 0.40, 1.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -27,7 +27,7 @@ static int str_put(char *no_str, char *str){
 int format(char *tmp, op2str op1, op2ch op2, const char *fmt, va_list ap){
 		char buf[16] = {0};
 		while(*fmt){
-				//putch(*fmt);
+				putch(*fmt);
 				//putch('\n');
 				switch(*fmt){
 						case '%':fmt++;break;
