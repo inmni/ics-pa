59ae5a9441e87d7e10709f580b4d1bde1716e49b
tracer-ics2022 <tracer@njuics.org>
2022-10-01 17:28:45 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:28:45 up 1 day, 20:42,  9 users,  load average: 0.32, 2.69, 2.71

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,6 +8,7 @@ typedef int (*op2str)(char *str1, char *str2);
 typedef int (*op2ch)(char *str, char ch);
 static int ch_copy(char *dst, char ch){
 		*(dst+1)=ch;
+		putch(ch);
 		return 1;
 }
 static int ch_put(char *no_str, char ch){
