1bb9bbef39c5a6860fe9ea1e970dfc3e18c6c9ea
tracer-ics2022 <tracer@njuics.org>
2022-10-16 00:12:44 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:12:44 up  2:35,  3 users,  load average: 2.24, 2.22, 1.61

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -96,7 +96,7 @@ static int decode_exec(Decode *s) {
 			);//y
 	INSTPAT(00000000000000000001000000010011, slli	 , IS, imm = imm&63;if(imm<=31)R(dest) = src1<<imm );//y
 	INSTPAT(00000000000000000101000000010011, srli	 , IS, imm = imm&63;if(imm<=31)R(dest) = src1>>imm );//y
-	INSTPAT(01000000000000000101000000010011, srai   , IS, imm = imm&63;if(imm<=31)R(dest) = ((int)src1)>>imm,printf("srai\n"));//y
+	INSTPAT(01000000000000000101000000010011, srai   , IS, imm = imm&63;if(imm<=31)R(dest) = ((int)src1)>>imm);//y
 		INSTPAT(00000000000000000000000000000011, lb		 , I, R(dest) = Mr(src1 + imm, 1); if(R(dest)&0x80)R(dest)|=0xFFFFFFF0);
 		INSTPAT(00000000000000000001000000000011, lh		 , I, R(dest) = Mr(src1 + imm, 2); if(R(dest)&0x8000)R(dest)|=0xFFFF0000);//y
   	INSTPAT(00000000000000000010000000000011, lw     , I, R(dest) = Mr(src1 + imm, 4));//y
