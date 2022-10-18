878c5937617dbc34ce77e6251daf1363a96b41a4
tracer-ics2022 <tracer@njuics.org>
2022-10-18 18:08:19 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:08:19 up 56 min,  1 user,  load average: 0.41, 0.98, 1.19

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -19,8 +19,7 @@ void sdb_mainloop();
 
 void engine_start() {
 #ifdef CONFIG_TARGET_AM
-  			printf("test\n");
-				cpu_exec(-1);
+  cpu_exec(-1);
 #else
   /* Receive commands from user. */
   sdb_mainloop();
