17d793db3d30b0d185aea3bb78a6e38d733ec676
tracer-ics2022 <tracer@njuics.org>
2022-10-18 18:00:05 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:00:05 up 47 min,  1 user,  load average: 0.77, 1.17, 1.35

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -30,8 +30,9 @@ CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
-
+#ifdef CONFIG_ITRACE_RING
 IRB iRB;
+#endif
 
 void device_update();
 #ifdef CONFIG_WATCHPOINT
