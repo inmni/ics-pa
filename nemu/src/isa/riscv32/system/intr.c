eff2875b9b23526cce1a286bea8880358cc963aa
211240032-Huang Wenyuan <211240032@smail.nju.edu.cn>
2022-10-19 21:24:20 +0800

    back to pa2 for report

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,13 +14,17 @@
 ***************************************************************************************/
 
 #include <isa.h>
-
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
-  return 0;
+TODO();
+//These are wrong
+				Event e; e.event = NO;
+	Context c;
+	c.mepc = epc;
+	user_handler(e, &c);
+  return (uint32_t)c.pdir;
 }
 
 word_t isa_query_intr() {
