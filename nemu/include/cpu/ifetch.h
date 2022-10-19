c35f78c84dbadd84edb4758cf151dc7f75ff89b2
tracer-ics2022 <tracer@njuics.org>
2022-10-19 17:10:30 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:10:30 up 50 min,  1 user,  load average: 1.36, 0.62, 0.49

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -20,7 +20,7 @@
 //IRB iRingBuffer;
 //void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 
-inline uint32_t inst_fetch(vaddr_t *pc, int len) {
+uint32_t inst_fetch(vaddr_t *pc, int len) {
   uint32_t inst = vaddr_ifetch(*pc, len);
 	(*pc) += len;
   return inst;
