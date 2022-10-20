a1cdfe0b84a6d5d91637b9462bf1774e146f2dc9
tracer-ics2022 <tracer@njuics.org>
2022-10-20 15:21:27 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:21:27 up 17:58,  7 users,  load average: 0.22, 0.56, 0.74

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,6 +5,7 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+	printf("aaa\n");
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
