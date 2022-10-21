2091197068e88a72d5e585980eca1a224081b69d
tracer-ics2022 <tracer@njuics.org>
2022-10-21 14:25:54 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:25:54 up 1 day, 17:02,  7 users,  load average: 0.25, 0.09, 0.11

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -64,8 +64,8 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-	extern char end;
-	static intptr_t pb = (intptr_t)(&end);
+	extern char _end;
+	static intptr_t pb = (intptr_t)(&_end);
 	if(_syscall_(SYS_brk, pb+increment, 0, 0)){
 			pb += increment;
 			return (void *)(pb-increment);
