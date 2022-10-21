db0a639730892771b9c9eee78ff98589686ae0a9
tracer-ics2022 <tracer@njuics.org>
2022-10-21 09:57:40 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:57:40 up 1 day, 12:34,  7 users,  load average: 0.63, 0.76, 0.63

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,6 +5,7 @@ static const char *syscall_table[] = {
 "SYS_exit", "SYS_yield",/*Others need implement*/
 };
 #endif
+uint32_t temp;
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -17,7 +18,13 @@ void do_syscall(Context *c) {
   switch (a[0]) {
 		case SYS_exit:	halt(a[1]);						break;
 		case SYS_yield:	yield();		c->GPRx=0;break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
+		case SYS_write: {
+				if(a[1]==1||a[1]==2){
+						for(temp = a[3];temp>0;temp--)putch(*((char *)a[2]+temp));
+						c->GPRx = 0;									break;
+				}
+		}
+		default: panic("Unhandled syscall ID = %d", a[0]);
   }
 #ifdef CONFIG_STRACE
 	printf(" ret %d\n",c->GPRx);
