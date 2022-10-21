4bd486fbaddd25d82a442bd052cfc0b241c18975
tracer-ics2022 <tracer@njuics.org>
2022-10-21 10:58:37 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:58:37 up 1 day, 13:35,  7 users,  load average: 0.90, 0.71, 0.69

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,7 +14,7 @@ void do_syscall(Context *c) {
 	a[2] = c->GPR3;
 	a[3] = c->GPR4;
 #ifdef CONFIG_STRACE
-	printf("SYScall trace: %s(%d %d %d)\n",syscall_table[a[0]],a[1],a[2],a[3]);
+	printf("syscall trace: %s(%d %d %d)\n",syscall_table[a[0]],a[1],a[2],a[3]);
 #endif
   switch (a[0]) {
 		case SYS_exit:	halt(a[1]);						break;
@@ -22,13 +22,13 @@ void do_syscall(Context *c) {
 		case SYS_write: {
 				if(a[1]==1||a[1]==2){
 						//putstr((char *)a[2]);
-						for(temp = 0;temp<13;temp++)putch(*((char *)a[2]+temp));
+						for(temp = 0;temp<1;temp++)putch(*((char *)a[2]+temp));
 						c->GPRx = 0;									break;
 				}
 		}
 		default: panic("Unhandled syscall ID = %d", a[0]);
   }
 #ifdef CONFIG_STRACE
-	printf("%s -> %d\n", syscall_table[a[0]],c->GPRx);
+	printf("\nsyscall trace: %s -> %d\n", syscall_table[a[0]],c->GPRx);
 #endif
 }
