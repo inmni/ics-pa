44d477d25b23f798d3c0e2b1f90741bc2bf1bde5
tracer-ics2022 <tracer@njuics.org>
2022-10-21 10:52:29 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:52:29 up 1 day, 13:29,  7 users,  load average: 0.29, 0.57, 0.70

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -22,7 +22,7 @@ void do_syscall(Context *c) {
 		case SYS_write: {
 				if(a[1]==1||a[1]==2){
 						putstr((char *)a[2]);
-						for(temp = 0;temp<a[3];temp++)putch(*((char *)a[2]+temp));
+						//for(temp = 0;temp<a[3];temp++)putch(*((char *)a[2]+temp));
 						c->GPRx = 0;									break;
 				}
 		}
