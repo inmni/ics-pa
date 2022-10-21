a6adeffd3b699a9e5986e6fe91ec76da3e4c7c7f
tracer-ics2022 <tracer@njuics.org>
2022-10-21 17:21:31 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:31 up 1 day, 19:58,  7 users,  load average: 1.00, 0.92, 0.94

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,6 +35,7 @@ void do_syscall(Context *c) {
 						c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
 				}																						break;
 		}
+		case SYS_close: c->GPRx = fs_close(a[1]);				break;
 		case SYS_lseek:	c->GPRx = fs_lseek(a[1], a[2], a[3]);
 																										break;
 		case SYS_brk:								c->GPRx=0;					break;
