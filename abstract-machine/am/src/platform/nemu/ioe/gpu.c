75f8ce92cd2e0818ce2045a0f6900425571471b6
tracer-ics2022 <tracer@njuics.org>
2022-10-23 21:20:52 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:20:52 up  5:00,  7 users,  load average: 0.62, 0.92, 0.76

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,19 +30,20 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-	/*int row,col;
+	int row,col;
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR+SCREEN_W*ctl->y+ctl->x;
 	uint32_t *pixels = (uint32_t *)ctl->pixels;
 	uint64_t *tmp1, *tmp2;
-	row = ctl->h;*/
+	row = ctl->h;
+				/*
 	uintptr_t fb = (uintptr_t)FB_ADDR + (SCREEN_W*ctl->y<<2) + (ctl->x<<2);
 	uintptr_t pixels = (uintptr_t)ctl->pixels;
 	while(ctl->h--){
 			memcpy((void *)fb, (void *)pixels, ctl->w << 2);
 			fb += SCREEN_W<<2; pixels += ctl->w << 2;
 	}
-	return;
-	/*
+	return;*/
+	
 	if(ctl->w & 1){
 		while(row--){
 			col = ctl->w>>1;
@@ -59,7 +60,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 			while(col--)*tmp1++ = *tmp2++;
 			fb+=SCREEN_W; pixels+=ctl->w;
 		}
-	}*/
+	}
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
