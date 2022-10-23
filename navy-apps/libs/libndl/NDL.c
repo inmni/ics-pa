7ebecf94bf92115477f18d6df75989ce7ca123d8
tracer-ics2022 <tracer@njuics.org>
2022-10-23 16:58:42 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:58:42 up 38 min,  7 users,  load average: 0.31, 0.38, 0.44

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -22,8 +22,7 @@ int NDL_PollEvent(char *buf, int len) {
 
 void NDL_OpenCanvas(int *w, int *h) {
   if (getenv("NWM_APP")) {
-    int fbctl = 4;
-    fbdev = 5;
+    int fbctl = open("/proc/dispinfo", 0, 0);
     screen_w = *w; screen_h = *h;
     char buf[64];
     int len = sprintf(buf, "%d %d", screen_w, screen_h);
