6ef638f64c2e63392fd708ea826be831ad744942
tracer-ics2022 <tracer@njuics.org>
2022-10-24 18:20:24 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:20:24 up  6:07,  7 users,  load average: 0.79, 0.76, 1.16

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,9 +1,7 @@
 #include <am.h>
-
+#include <stdio.h>
 Area heap;
 
-void putch(char ch) {
-}
+void putch(char ch) { outb(SERIAL_PORT, ch);}
 
-void halt(int code) {
-}
+void halt(int code) { asm volatile("mv a0, %0; ebreak" : :"r"(code)); }
