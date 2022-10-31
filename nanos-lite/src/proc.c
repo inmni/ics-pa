98c67af3bc01eeb3bb3a7f36b902c9391cdf6044
tracer-ics2022 <tracer@njuics.org>
2022-10-31 14:02:49 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:02:49 up 4 days,  9:54,  7 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,14 +22,14 @@ void hello_fun(void *arg) {
 
 void naive_uload(PCB *pcb, const char *filename);
 void init_proc() {
-//	context_kload(&pcb[0], hello_fun, "AAAAAA");
-	//context_kload(&pcb[1], hello_fun, "ZZZZZZ");
+	context_kload(&pcb[0], hello_fun, "AAAAAA");
+	context_kload(&pcb[1], hello_fun, "ZZZZZZ");
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-	 naive_uload(NULL, "/bin/menu");
+	// naive_uload(NULL, "/bin/menu");
 }
 
 void context_kload(PCB* p, void (*entry)(void *), void* arg) {
