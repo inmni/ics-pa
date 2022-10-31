3b9b962fb6127bc7daf78f0006224de4589f32b3
Inmny <211240032@smail.nju.edu.cn>
2022-10-31 15:32:47 +0800

    modify navy test: hello

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,17 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
+	char *strToPrint = NULL;
+	if(argc>0 && argv){
+			strToPrint = argv[0];
+	}
   while (1) {
     j ++;
     if (j == 10000) {
 			//write(1, "Hello World from Navy-apps for the ",36);
 			//printf("%d", i ++);
 			//write(1," th times!\n",11);
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      printf("%s for the %dth time!\n", strToPrint, i ++);
       j = 0;
     }
   }
