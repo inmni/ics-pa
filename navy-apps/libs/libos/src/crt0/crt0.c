a84d45da16447f1c8bad10f2e032ba92781de36e
tracer-ics2022 <tracer@njuics.org>
2022-10-31 16:05:23 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:23 up 4 days, 11:57,  7 users,  load average: 0.88, 0.49, 0.28

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,12 +7,10 @@ extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-	printf("The address of args: %p\n", args);
 	int argc = *(int *)args;
 	char **argv = (char **)(args + 1);
 	char **envp = (char **)(args + 2);
   environ = envp;
-	printf("The args are %d, %p, %p\n", argc, argv, envp);
 	exit(main(argc, argv, envp));
   assert(0);
 }
