ca7c2b11765ab6af21fdbaf9cbb915a077f33e5a
tracer-ics2022 <tracer@njuics.org>
2022-11-01 19:13:01 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:13:01 up 5 days,  6:36,  7 users,  load average: 0.17, 0.26, 0.24

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -81,7 +81,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	if(prot)printf("start map va[%08x]->pa[%08x] with prot[%x]\n", (uintptr_t)va, (uintptr_t)pa, prot);
 	// LEVEL 1
 	PTE *pte = as->ptr + VPN_1(va)*PTESIZE;
-	if(prot)printf("PTE addr: %p, value: %08x\n", pte, 0);
+	if(prot)printf("PTE addr: %08x, value: %08x\n", (uintptr_t)pte, 0);
 	// if the pte is not valid
 	if(!(*pte & PTE_V)){
 		// alloc leaf page
