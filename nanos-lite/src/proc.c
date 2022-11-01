dfb1330a2edadf47a4a1be1ac5587d94e8fe7964
tracer-ics2022 <tracer@njuics.org>
2022-11-01 21:59:19 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:59:19 up 5 days,  9:22,  7 users,  load average: 0.35, 0.27, 0.15

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,12 +25,12 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-	context_kload(&pcb[0], hello_fun, "AAAAAA");
+//	context_kload(&pcb[0], hello_fun, "AAAAAA");
 	//context_kload(&pcb[1], hello_fun, "ZZZZZZ");
 	char *arg1[] = {"ZZZZZZ", NULL};
-//	char *arg2[] = {"/bin/cat", "/share/games/bird/atlas.txt", NULL};
+	char *arg2[] = {"/bin/cat", "/share/games/bird/atlas.txt", NULL};
 	char *empty[] = {NULL};
-//  context_uload(&pcb[0], "/bin/hello", arg2, empty);
+  context_uload(&pcb[0], "/bin/hello", arg2, empty);
 //	context_uload(&pcb[1], "/bin/hello", arg2, empty);
 //	printf("arg1: %s, arg2: %s\n", arg1[0], arg2[0]);
 	context_uload(&pcb[1], "/bin/hello", arg1, empty);
