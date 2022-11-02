d8a2665877482e35b19337f86ed261eaaa6ba939
tracer-ics2022 <tracer@njuics.org>
2022-11-02 14:04:20 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:04:20 up 5 days, 16:31,  7 users,  load average: 0.32, 0.49, 0.49

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,15 +29,15 @@ void hello_fun(void *arg) {
 
 void init_proc() {
 	//memset(pcb, 0, sizeof(pcb));
-	//context_kload(&pcb[0], hello_fun, "AAAAAA");
+	context_kload(&pcb[0], hello_fun, "AAAAAA");
 	//context_kload(&pcb[1], hello_fun, "ZZZZZZ");
 	char *arg1[] = {"ZZZZZZ", NULL};
 //	char *arg2[] = {"/bin/cat", "/share/games/bird/atlas.txt", NULL};
 	char *empty[] = {NULL};
-  context_uload(&pcb[0], "/bin/hello", arg1, empty);
-	context_uload(&pcb[1], "/bin/nterm", arg1, empty);
+  //context_uload(&pcb[0], "/bin/hello", arg1, empty);
+	//context_uload(&pcb[1], "/bin/nterm", arg1, empty);
 //	printf("arg1: %s, arg2: %s\n", arg1[0], arg2[0]);
-//	context_uload(&pcb[1], "/bin/hello", arg1, empty);
+	context_uload(&pcb[1], "/bin/hello", arg1, empty);
 	switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -59,9 +59,9 @@ Context* schedule(Context *prev) {
 	do{
 		curr_pcb_id++;
 		curr_pcb_id %= MAX_NR_PROC;
-		//printf("check %d prio: %d\n", curr_pcb_id, pcb[curr_pcb_id].prio);
+		printf("check %d prio: %d\n", curr_pcb_id, pcb[curr_pcb_id].prio);
 	} while(pcb[curr_pcb_id].prio==0);
-//	printf("schedule to %d\n", curr_pcb_id);
+	printf("schedule to %d\n", curr_pcb_id);
 	//current = &pcb[curr_pcb_id]; // Need to change
 	current = (current == &pcb[0] ? &pcb[1]:&pcb[0]);
   return current->cp;
