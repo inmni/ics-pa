0e0c503b4aa2ecffc9f8f9b5b186aea38b0acfa1
tracer-ics2022 <tracer@njuics.org>
2022-11-02 14:19:02 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:19:02 up 5 days, 16:46,  7 users,  load average: 0.38, 0.68, 0.70

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -59,9 +59,9 @@ Context* schedule(Context *prev) {
 	do{
 		curr_pcb_id++;
 		curr_pcb_id %= MAX_NR_PROC;
-		printf("check %d prio: %d\n", curr_pcb_id, pcb[curr_pcb_id].prio);
+		//printf("check %d prio: %d\n", curr_pcb_id, pcb[curr_pcb_id].prio);
 	} while(pcb[curr_pcb_id].prio==0);
-	printf("schedule to %d\n", curr_pcb_id);
+	//printf("schedule to %d\n", curr_pcb_id);
 	//current = &pcb[curr_pcb_id];
 	current = (current == &pcb[0] ? &pcb[1]:&pcb[0]);
   return current->cp;
