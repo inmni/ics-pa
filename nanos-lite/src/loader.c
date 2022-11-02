8a3e786fb0044ddcd926dba89b217309f57be5c1
tracer-ics2022 <tracer@njuics.org>
2022-11-02 13:33:41 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:33:41 up 5 days, 16:16,  7 users,  load average: 0.26, 0.28, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -111,5 +111,6 @@ void context_uload(PCB* p, const char *filename, char *const argv[], char *const
 	p->cp = ucontext(&(p->as), kstack, (void *)entry);
 	p->cp->GPRx = (uintptr_t)ustack;
 	p->prio = 1;
+	printf("prio set:%d, addr: %p\n", p->prio, &p->prio);
 //	printf("args begin: %p, argc: %d, argv begin: %p, argv[0] value: %s\n", ustack, *(uint32_t *)ustack, ustack + 4, *(char **)(ustack + 4));
 }
