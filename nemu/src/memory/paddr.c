950359da2f77b210693d5711e9cdc6366e251efa
tracer-ics2022 <tracer@njuics.org>
2022-11-02 15:34:38 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:34:38 up 35 min,  5 users,  load average: 0.68, 0.49, 0.44

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,8 +51,8 @@ static void out_of_bound(paddr_t addr) {
 	}while(tmp!=mrwb.st_index && tmp<mrwb.cur_len);
 	printf("Latest memory operations have been stored in nemu-log\n");
 #endif
-	nemu_state.state=NEMU_ABORT;
-  printf("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+  printf("aaaa\n");
+	panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
 
