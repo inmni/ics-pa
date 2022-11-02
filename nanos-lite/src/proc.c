920ddb835243bce3c35d89514fda0c86b3dc2860
tracer-ics2022 <tracer@njuics.org>
2022-11-02 09:16:35 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:16:35 up 5 days, 11:59,  7 users,  load average: 0.58, 0.34, 0.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -18,7 +18,7 @@ void hello_fun(void *arg) {
   int j = 1;
   while (1) {
     if(j%10000==0){
-			Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (char *)arg, j);
+			Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (char *)arg, j/10000);
 		}
 		j ++;
   }
