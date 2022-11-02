6fc3c78daa6f1c0da8bba362a53bf09825045ef1
tracer-ics2022 <tracer@njuics.org>
2022-11-02 14:02:43 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:02:43 up 5 days, 16:30,  7 users,  load average: 0.29, 0.55, 0.51

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,15 +29,15 @@ void hello_fun(void *arg) {
 
 void init_proc() {
 	//memset(pcb, 0, sizeof(pcb));
-	context_kload(&pcb[0], hello_fun, "AAAAAA");
+	//context_kload(&pcb[0], hello_fun, "AAAAAA");
 	//context_kload(&pcb[1], hello_fun, "ZZZZZZ");
 	char *arg1[] = {"ZZZZZZ", NULL};
 //	char *arg2[] = {"/bin/cat", "/share/games/bird/atlas.txt", NULL};
 	char *empty[] = {NULL};
-  //context_uload(&pcb[0], "/bin/hello", arg2, empty);
-//	context_uload(&pcb[1], "/bin/hello", arg2, empty);
+  context_uload(&pcb[0], "/bin/hello", arg1, empty);
+	context_uload(&pcb[1], "/bin/nterm", arg1, empty);
 //	printf("arg1: %s, arg2: %s\n", arg1[0], arg2[0]);
-	context_uload(&pcb[1], "/bin/hello", arg1, empty);
+//	context_uload(&pcb[1], "/bin/hello", arg1, empty);
 	switch_boot_pcb();
 
   Log("Initializing processes...");
