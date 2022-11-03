7302f71c26d5c43679b6aa2ee743e4d1fc405c23
tracer-ics2022 <tracer@njuics.org>
2022-11-03 09:40:32 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-INMNY 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:40:32 up 18:09,  6 users,  load average: 0.13, 0.39, 0.23

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,7 +1,7 @@
 #include <common.h>
 #include <device.h>
 #include <fs.h>
-#undef MULTIPROGRAM
+#define TIME_SHARING
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
 # define MULTIPROGRAM_YIELD() yield()
 #else
