2fbb1d0532af8604cd5917f2cac3926451c20cff
tracer-ics2022 <tracer@njuics.org>
2022-11-15 22:27:26 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-Inmny 5.15.68.1-microsoft-standard-WSL2 #1 SMP Mon Sep 19 19:14:52 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:27:26 up  9:07,  1 user,  load average: 0.15, 0.11, 0.09

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <memory/host.h>
 #include <memory/paddr.h>
@@ -19,82 +19,98 @@
 #include <isa.h>
 
 #ifdef CONFIG_MTRACE
-	#include <trace.h>
-	M_RW_B mrwb;
+#include <trace.h>
+M_RW_B mrwb;
 #endif
 
-#if   defined(CONFIG_PMEM_MALLOC)
+#if defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
-uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+uint8_t *guest_to_host(paddr_t paddr)
+{
+	return pmem + paddr - CONFIG_MBASE;
+}
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-static word_t pmem_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host(addr), len);
-  return ret;
+static word_t pmem_read(paddr_t addr, int len)
+{
+	word_t ret = host_read(guest_to_host(addr), len);
+	return ret;
 }
 
-static void pmem_write(paddr_t addr, int len, word_t data) {
-  host_write(guest_to_host(addr), len, data);
+static void pmem_write(paddr_t addr, int len, word_t data)
+{
+	host_write(guest_to_host(addr), len, data);
 }
 
-static void out_of_bound(paddr_t addr) {
+static void out_of_bound(paddr_t addr)
+{
 #ifdef CONFIG_MTRACE
 	int tmp = mrwb.st_index;
-	do{
+	do
+	{
 		log_write("%s\n", mrwb.buf[tmp]);
 		tmp++;
-		tmp%=MAX_NR_MRWB;
-	}while(tmp!=mrwb.st_index && tmp<mrwb.cur_len);
+		tmp %= MAX_NR_MRWB;
+	} while (tmp != mrwb.st_index && tmp < mrwb.cur_len);
 	printf("Latest memory operations have been stored in nemu-log\n");
 #endif
 	panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
-      addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+		  addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
 
-void init_mem() {
-#if   defined(CONFIG_PMEM_MALLOC)
-  pmem = malloc(CONFIG_MSIZE);
-  assert(pmem);
+void init_mem()
+{
+#if defined(CONFIG_PMEM_MALLOC)
+	pmem = malloc(CONFIG_MSIZE);
+	assert(pmem);
 #endif
 #ifdef CONFIG_MEM_RANDOM
-  uint32_t *p = (uint32_t *)pmem;
-  int i;
-  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
-    p[i] = rand();
-  }
+	uint32_t *p = (uint32_t *)pmem;
+	int i;
+	for (i = 0; i < (int)(CONFIG_MSIZE / sizeof(p[0])); i++)
+	{
+		p[i] = rand();
+	}
 #endif
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+	Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
-word_t paddr_read(paddr_t addr, int len) {
+word_t paddr_read(paddr_t addr, int len)
+{
 	word_t data = 0;
-  if (likely(in_pmem(addr))){
-			data = pmem_read(addr, len);
+	if (likely(in_pmem(addr)))
+	{
+		data = pmem_read(addr, len);
 #ifdef CONFIG_MTRACE
-			buf_mem_op(&mrwb, addr, len, data, M_READ);
+		buf_mem_op(&mrwb, addr, len, data, M_READ);
 #endif
-			return data;
+		return data;
 	}
 #ifdef CONFIG_DEVICE
-	  data = mmio_read(addr, len);
+	data = mmio_read(addr, len);
 #ifdef CONFIG_MTRACE
-									buf_mem_op(&mrwb, addr, len, data, M_READ);
-#endif	
-		return data;
+	buf_mem_op(&mrwb, addr, len, data, M_READ);
 #endif
-  out_of_bound(addr);
-  return 0;
+	return data;
+#endif
+	out_of_bound(addr);
+	return 0;
 }
 
-void paddr_write(paddr_t addr, int len, word_t data) {
+void paddr_write(paddr_t addr, int len, word_t data)
+{
 #ifdef CONFIG_MTRACE
 	buf_mem_op(&mrwb, addr, len, data, M_WRITE);
 #endif
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return); 
-  out_of_bound(addr);
+	if (likely(in_pmem(addr)))
+	{
+		pmem_write(addr, len, data);
+		return;
+	}
+	IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return );
+	out_of_bound(addr);
 }
