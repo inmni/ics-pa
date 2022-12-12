eb8950dc4939a97395cecd17cc1b6d6f6afb62f6
tracer-ics2022 <tracer@njuics.org>
2022-12-12 17:53:31 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-Inmny 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:53:31 up  1:43,  1 user,  load average: 0.07, 0.07, 0.08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -26,7 +26,7 @@ void do_syscall(Context *c) {
 	a[3] = c->GPR4;
 #ifdef CONFIG_STRACE
 	printf("===============syscall trace start================\n");
-	printf("syscall trace: %s(%d %d %d)\n",syscall_table[a[0]],a[1],a[2],a[3]);
+	printf("syscall trace: %s(%d %08x %d)\n",syscall_table[a[0]],a[1],a[2],a[3]);
 #endif
   switch (a[0]) {
 		case SYS_exit:	naive_uload(NULL, "/bin/menu"); c->GPRx=0;	//halt(a[1]);
