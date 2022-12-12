c120bcb8cbc17936c5b031f23519a85b4a4fc57b
tracer-ics2022 <tracer@njuics.org>
2022-12-12 17:28:46 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-Inmny 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:28:46 up  1:19,  1 user,  load average: 0.12, 0.06, 0.08

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -1,13 +1,15 @@
 #include <NDL.h>
 #include <sdl-timer.h>
 #include <stdio.h>
-
+#include <assert.h>
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
-  return NULL;
+  assert(0);
+	return NULL;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
-  return 1;
+  assert(0);
+	return 1;
 }
 
 uint32_t SDL_GetTicks() {
@@ -15,4 +17,5 @@ uint32_t SDL_GetTicks() {
 }
 
 void SDL_Delay(uint32_t ms) {
+	assert(0);
 }
