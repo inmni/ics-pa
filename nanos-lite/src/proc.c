b95bba7e43fa717e44416b2f9b2eb2e81e4a3452
tracer-ics2022 <tracer@njuics.org>
2023-01-11 12:36:32 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-Inmny 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:36:32 up  3:24,  1 user,  load average: 0.18, 0.17, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -43,8 +43,8 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg);
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 
 #define PROG_PATH1 "/bin/menu"
-#define PROG_PATH2 "/bin/menu"
-#define PROG_PATH3 "/bin/menu"
+#define PROG_PATH2 "/bin/pal"
+#define PROG_PATH3 "/bin/bird"
 void init_proc() {
 	context_kload(&pcb[0], hello_fun, "ONE");
   char *argv1[] = {PROG_PATH1, NULL};
