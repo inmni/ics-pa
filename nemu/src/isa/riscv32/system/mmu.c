6868c68dd486fab49b0cc03006f30c4d1ab5ea16
211240032-Huang Wenyuan <1342885270@qq.com>
2023-01-11 11:21:54 +0800

    test error vaddr&0x10000000 on oj

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -68,7 +68,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   //   Log("page_table_entry not valid, vaddr: %#x", vaddr);
   // }
 	if(!(page_table_entry & PTE_V)){
-		Assert(vaddr & 0x8000000, "");
 		Assert(vaddr & 0x10000000, "");
 		Assert(vaddr & 0x20000000, "");
 		Assert(vaddr & 0x40000000, "");
