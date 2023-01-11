ded477a9c0762ddbf1b4802e36a064e116437910
tracer-ics2022 <tracer@njuics.org>
2023-01-11 16:31:13 +0800

    >  compile NEMU 211240032 黄文橼 Linux PC-Inmny 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:31:13 up  7:19,  1 user,  load average: 0.09, 0.08, 0.02

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,21 +1,20 @@
 #include <memory.h>
-#include <stdint.h>
 #include <proc.h>
-
 static void *pf = NULL;
-
 void* new_page(size_t nr_page) {
-  //nr_page * 4KB
-  pf += nr_page * 4 * 1024;
-  return pf - nr_page * 4 * 1024;
+	assert(pf + nr_page*PGSIZE <= heap.end);
+	void *pf_start = pf;
+	//printf("new page [%08x, %08x)\n", (uintptr_t)pf_start, (uintptr_t)pf_start + nr_page*PGSIZE);
+	memset(pf_start, 0, nr_page * PGSIZE);
+	pf += nr_page * PGSIZE;
+	return pf_start;
 }
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-  assert(n % (4096) == 0);
-  void *ret = new_page(n / (4096));
-  memset(ret, 0, n);
-  return ret;
+  void *pg =  new_page(n/PGSIZE);
+	memset(pg, 0, n);
+	return pg;
 }
 #endif
 
@@ -23,30 +22,23 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
-//#define align4k_page_number(x) (x % 4096 == 0 ? x / 4096 : (x / 4096 + 1))
-
-extern PCB *current;
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
-  uintptr_t max_page_end = current->max_brk; 
-  uintptr_t max_page_pn = (max_page_end >> 12) - 1;
-  uintptr_t brk_pn = brk >> 12;//12
-
-  //[page_start, page_end) 所以，应该是大于等于就重新分配
-  //printf("申请内存 max_page_end: %p\t to brk: %p\n", max_page_end, brk);//分配从[max_page_pn, brk_pn]的所有页
-  if (brk >= max_page_end){
-    void *allocted_page =  new_page(brk_pn - max_page_pn + 1);//需要申请的新的页数
-    for (int i = 0; i < brk_pn - max_page_pn + 1; ++i){
-      //TODO: prot 有问题
-      map(&current->as, (void *)(max_page_end + i * 0xfff),
-       (void *)(allocted_page + i * 0xfff), 1);
-    }
-
-    current->max_brk = (brk_pn + 1) << 12;
-    assert(current->max_brk > brk);
-  }
-
-  return 0;
+  current->max_brk = ROUNDUP(current->max_brk, PGSIZE);
+	if(brk <= current->max_brk)	return 0;
+	// alloc
+	uint32_t pg_nr = ROUNDUP(brk - current->max_brk, PGSIZE)/PGSIZE;
+	//Log("brk: %08x - curr->max_brk: %08x = %d pages to alloc in mm_brk", brk, current->max_brk, pg_nr);
+	void* pg_start = new_page(pg_nr);	int i;
+	for(i = 0; i<pg_nr; i++){
+		map(&current->as,
+			(void *)current->max_brk + i*PGSIZE,
+			pg_start + i*PGSIZE,
+			MMAP_READ | MMAP_WRITE
+			);
+	}
+	current->max_brk += pg_nr * PGSIZE;
+	return 0;
 }
 
 void init_mm() {
